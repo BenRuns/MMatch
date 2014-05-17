dc6c65190948f6b7d950fb017dfbb33f1e10f5ea
sealocal <local.mat@gmail.com>
2014-05-17 14:07:20 -0700

    rails g conroller Pages home about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MakerMatch::Application.routes.draw do
+  get "pages/home"
+  get "pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
