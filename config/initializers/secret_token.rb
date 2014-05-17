fe84c668ee2ec89247eedb144fb393a692d4bcc1
sealocal <local.mat@gmail.com>
2014-05-17 13:49:26 -0700

    rails new [app]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakerMatch::Application.config.secret_key_base = 'fa70740ef9ee8bc1b58b7a0757918898cc023b80176de5eaceff5e10709cb3b9abd3f905ab491d5f387bd8b6dc2e2b3a38463d0f688f97f5bb59d5fd826a9a05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MakerMatch::Application.config.session_store :cookie_store, key: '_maker-match_session'
