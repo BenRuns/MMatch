87f9ed2f2ee3ab5743a36b2b8bc53046b7d1947d
sealocal <local.mat@gmail.com>
2014-10-18 02:01:52 -0700

    add question methods to Space model

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,2 +1,21 @@
 class Space < ActiveRecord::Base
+  def laser_cutter?
+    laser_cutter
+  end
+
+  def three_d_printer?
+    three_d_printer
+  end
+
+  def cnc_router?
+    cnc_router
+  end
+
+  def sewing_maching?
+    sewing_maching
+  end
+
+  def microcontroller_library?
+    microcontroller_library
+  end
 end
